fed38b22f3c1226e73210fecdf75761d5ce4a384
Adnan Reza <adnan.reza@outlook.com>
2020-04-26 17:01:54 -0700

    Scramble list items

--- script.js
+++ script.js
@@ -18,17 +18,26 @@ createList();
 
 // Insert List items into DOM
 function createList() {
-  [...tvShows].forEach((show, index) => {
-    const li = document.createElement('li');
-    li.setAttribute('data-index', index);
-    li.innerHTML = `
+  //Copy tvShows array using spread operator
+  [...tvShows]
+    //Transform each item in array into object with value and sort number
+    .map((item) => ({ value: item, sort: Math.random() }))
+    //Sort by random value
+    .sort((a, b) => a.sort - b.sort)
+    //Turn it back into array of strings
+    .map((item) => item.value)
+    //Loop through each show and generate li
+    .forEach((show, index) => {
+      const li = document.createElement('li');
+      li.setAttribute('data-index', index);
+      li.innerHTML = `
     <span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
       <p class="show-name">${show}</p>
       <i class="fas fa-grip-lines"></i>
     </div>
     `;
-    listItems.push(li);
-    draggable_list.appendChild(li);
-  });
+      listItems.push(li);
+      draggable_list.appendChild(li);
+    });
 }
